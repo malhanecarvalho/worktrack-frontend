52bf51f5a5b19573c4d06d35bf89a3c2320b9582
malhane carvalho <malhanec@gmail.com>
2025-01-28 19:35:13 +0000

    feat: add link do perfil github no rodapé

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      Desenvolvido por <a href="https://github.com/seuusuario" target="_blank" rel="noopener noreferrer">© Malhane Carvalho</a>
+      Desenvolvido por <a href="https://github.com/malhanecarvalho" target="_blank" rel="noopener noreferrer">© Malhane Carvalho</a>
     </footer>
   );
 };
